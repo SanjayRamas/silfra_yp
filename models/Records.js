2900081ba78c4b5168a756f3ae5a66ae0640b837
SanjayRamas <sansa091@gmail.com>
2017-08-18 10:51:00 +0530

    Add files via upload

---
+++ models/Records.js
@@ -0,0 +1,18 @@
+var mongoose = require('mongoose');
+
+var RecordSchema = new mongoose.Schema({
+  bizname: String,
+  address: String,
+  email: String,
+  phone: String,
+  category: String,
+  upvotes: {type: Number, default: 0},
+  comments: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Comment' }]
+});
+
+RecordSchema.methods.upvote = function (cb) {
+  this.upvotes+=1;
+  this.save(cb);
+}
+
+mongoose.model('Record', RecordSchema);

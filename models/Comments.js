43d28886ddfe9479a156a970eec8c18309bec899
System <none>
2017-08-18 05:16:58 +0000

    🏁😳 Updated with Glitch

---
+++ models/Comments.js
@@ -0,0 +1,15 @@
+var mongoose = require('mongoose');
+
+var CommentSchema = new mongoose.Schema ({
+  body: String,
+  author: String,
+  upvotes: {type: Number, default: 0},
+  record: { type: mongoose.Schema.Types.ObjectId, ref: 'Record' }
+});
+
+CommentSchema.methods.upvote = function (cb) {
+  this.upvotes++;
+  this.save(cb);
+}
+
+mongoose.model('Comment', CommentSchema);
